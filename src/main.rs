63bd80b6b61a11920abf2bda4fa664a0e12a9829
AgustinBadi <absalinas@uc.cl>
2024-04-10 11:38:18 -0400

    Prototyping storing state

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use serde::{Serialize, Deserialize};
 use std::io;
+use std::fs;
+use std::io::Write;
+use std::env;
 
+#[derive(Debug, Serialize, Deserialize, Default)]
+struct ProgramState {
+    user: String,
+    points: u64,
+    tasks: Vec<String>,
+    trash_can: Vec<String>,
+}
 
 fn main() {
+
     let mut quit = false;
 
-    let mut list: Vec<String> = Vec::new();
+
+    let mut list: Vec<String> = get_state_file().tasks;
 
 
     println!("Hi, I'm your todo manager! 💻");
@@ -28,6 +41,7 @@ fn main() {
             //list.pop();
             list = del_task(&list).to_vec();
         } else if input == "q" {
+            save_state(&list);
             println!("Bye bye.. 🖖");
             quit  = true;
         } else {
@@ -65,7 +79,6 @@ fn del_task(xs: &Vec<String>) -> Vec<String> {
         }
     };
 
-
     let mut new_tasks = Vec::new();
     let mut counter: u8 = 1; 
 
@@ -80,4 +93,50 @@ fn del_task(xs: &Vec<String>) -> Vec<String> {
 
     new_tasks
 }
+
+fn get_state_file() -> ProgramState  {
+    let current_dir = env::current_dir().unwrap();
+    
+    // Check if any file named "state.json" exists
+    let is_state_json_present = current_dir.iter()
+        .any(|entry| entry == "state.json");
+
+        
+    if is_state_json_present {
+        let json_string = fs::read_to_string("state.json").unwrap();
+
+        let my_data: ProgramState =  serde_json::from_str(&json_string).unwrap();
+
+        return my_data;
+    } else {
+        let new_data = serde_json::to_string(&ProgramState::default()).unwrap(); 
+
+        let _ = fs::File::create("state.json").unwrap().write_all(new_data.as_bytes());
+        return ProgramState::default();
+    }
+        
+}
+
+fn save_state(list: &Vec<String>) -> () {
+    let mut new_data = ProgramState::default();
+    new_data.tasks = list.to_vec();
+    let update_file = serde_json::to_string(&new_data).unwrap(); 
+
+
+    let _ = fs::File::create("state.json").unwrap().write_all(update_file.as_bytes());   
+}
+
+
+
+
+
+
+// 1. Add scores (Did I finish all task each day?)
+// 2. Implement State (JSON)
+// 3. Add sections (life, development, university, health, etc..).
+// 4. Authentication
+// 5. Encryption
+// 6. Add time stamps
+// 7. Trash can
+// 8. 
 
