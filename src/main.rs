62f1652a5325c1512794668a906959c152872a92
AgustinBadi <absalinas@uc.cl>
2024-08-27 18:28:51 -0400

    Add upgrades

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,37 @@ use std::io::Write;
 use std::env;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
+
+//struct TaskRecord {
+//    dots: Vec<u8>,
+//    day: ..,
+//}
+
 struct ProgramState {
     user: String,
     points: u64,
     tasks: Vec<String>,
-    trash_can: Vec<String>,
 }
+
+
+//    Implementar time dots con un Vector eg. [3,0,1,3,5,..] donde cada elemento es la cantidad de tareas diaras. 
+
+    
 
 fn main() {
 
     let mut quit = false;
 
+    let prog_state: ProgramState = get_state_file();
+    let name_state: String = prog_state.user; 
+    let mut points: u64 = prog_state.points;
+    let mut tasks: Vec<String> = prog_state.tasks;
 
-    let mut list: Vec<String> = get_state_file().tasks;
+    let name = get_name(name_state);
 
-
-    println!("Hi, I'm your todo manager! 💻");
-    println!("type 'p' to print, 'a' to add, 'd' to del, 'q' to quit.");
+    println!("Hi {}, I'm your todo manager! 💻", name);
+    println!("Your points are: {} 🔥", points);
+    println!("Type 'p' to print, 'a' to add, 'd' to del, 'f' to finish and 'q' to quit.");
 
     while quit == false {
         let mut input = String::new();
@@ -30,23 +44,42 @@ fn main() {
         input = input.trim().to_string();
 
         if input == "a" {
-            println!("Type the task...");
+            println!("Type the task...\n");
             input = "".to_string();
             let _ = io::stdin().read_line(&mut input);
             input = input.trim().to_string();
-            list.push(String::from(input));
+            tasks.push(String::from(input));
+            println!("Task added 👌");
         } else if input == "p" {
-            print_tasks(&list);
+            print_tasks(&tasks);
         } else if input == "d" {
-            //list.pop();
-            list = del_task(&list).to_vec();
+            //tasks.pop();
+            tasks = del_task(&tasks , false).to_vec();
+        } else if input == "f" {
+            tasks = del_task(&tasks, true).to_vec();
+            println!("+10 points! 💯");
+            points += 10;
         } else if input == "q" {
-            save_state(&list);
             println!("Bye bye.. 🖖");
             quit  = true;
         } else {
             println!("type 'p' to print, 'a' to add, 'd' to del, 'q' to quit.");
         }
+    }
+
+    save_state(name, points,  &tasks);
+}
+
+fn get_name(name: String) -> String {
+    let mut input = String::new();
+
+    if name == "" {
+        println!("Seems you are new uh? Type you name buddy...");
+        let _ = io::stdin().read_line(&mut input);
+        input = input.trim().to_string();
+        return input;
+    } else {
+        return name;
     }
 }
 
@@ -59,7 +92,7 @@ fn print_tasks(xs: &Vec<String>) -> () {
     }
 }
 
-fn del_task(xs: &Vec<String>) -> Vec<String> {
+fn del_task(xs: &Vec<String>, finished: bool) -> Vec<String> {
     println!("Which one?");
     print_tasks(xs);
 
@@ -86,7 +119,11 @@ fn del_task(xs: &Vec<String>) -> Vec<String> {
         if number != counter {
             new_tasks.push(String::from(task));
         } else {
-            println!("task {} moved to 🗑️", counter);
+            match finished {
+                false => println!("task {} moved to 🗑️", counter),
+                _ => println!("Great you finished the task!"),
+            }
+            
         }
         counter += 1;
     }
@@ -96,11 +133,13 @@ fn del_task(xs: &Vec<String>) -> Vec<String> {
 
 fn get_state_file() -> ProgramState  {
     let current_dir = env::current_dir().unwrap();
-    
-    // Check if any file named "state.json" exists
-    let is_state_json_present = current_dir.iter()
-        .any(|entry| entry == "state.json");
 
+    //println!("Current directory: {:?}", current_dir);
+    
+    let is_state_json_present = std::fs::read_dir(&current_dir).unwrap()
+    .any(|entry| entry.unwrap().file_name() == "state.json");
+
+    //println!("is_state_json_present: {}", is_state_json_present);
         
     if is_state_json_present {
         let json_string = fs::read_to_string("state.json").unwrap();
@@ -117,9 +156,11 @@ fn get_state_file() -> ProgramState  {
         
 }
 
-fn save_state(list: &Vec<String>) -> () {
+fn save_state(name: String, points: u64,  tasks: &Vec<String>) -> () {
     let mut new_data = ProgramState::default();
-    new_data.tasks = list.to_vec();
+    new_data.user = name;
+    new_data.tasks = tasks.to_vec();
+    new_data.points = points;
     let update_file = serde_json::to_string(&new_data).unwrap(); 
 
 
@@ -128,15 +169,22 @@ fn save_state(list: &Vec<String>) -> () {
 
 
 
-
-
-
 // 1. Add scores (Did I finish all task each day?)
+//  - Tasks: completed.
+//  - Day Streak: 
+//  - Points: +10 daily batch
 // 2. Implement State (JSON)
 // 3. Add sections (life, development, university, health, etc..).
 // 4. Authentication
 // 5. Encryption
 // 6. Add time stamps
 // 7. Trash can
-// 8. 
+
+// -- RPG Styled -- //
+
+// Make Paths of character development. 
+// Eg: Professional (Red), Personal (Green) and Vocacionally (Blue).
+// Professionally: Things you do to survive.
+// Personal: Things you do to grow.
+// Vocacionally: Things that u do for others.
 
